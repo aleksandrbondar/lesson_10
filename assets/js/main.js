0a73616169c26e56f56789cce610633d8b450389
aleksandrbondar <aleksandr_bondar@icloud.com>
2024-03-13 09:32:09 +0200

    SCSS v 2.1

--- assets/js/main.js
+++ assets/js/main.js
@@ -4,3 +4,19 @@ $(document).ready(function () {
     $('.navbar__list').slideToggle(800);
   });
 });
+
+function updateNavbar() {
+  switch (window.innerWidth > 768) {
+    case true:
+      document.querySelector('.navbar__list').style.display = 'flex';
+      break;
+    case false:
+      document.querySelector('.navbar__list').style.display = 'none';
+      document.querySelector('.navbar__button').classList.remove('open');
+      break;
+    default:
+      break;
+  }
+}
+
+window.addEventListener('resize', updateNavbar);
